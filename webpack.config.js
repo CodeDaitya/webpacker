dd0788c7c17f07f6e4227c9d27c3184a6b0b20a3
Deepak Maurya <itsdpm@gmail.com>
2020-03-09 19:07:48 +0530

    Added SASS loader, setup bootstrap override

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: [
+          "style-loader",   // #3 injects the style into DOM
+          "css-loader",     // #2 loads the css code and turns it into commonjs code
+          "sass-loader"     // #1 loads the scss code and turns it into css
+        ]
       }
     ]
   }
